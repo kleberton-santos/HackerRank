73dcc81e2483f55fcdd9e73710636d1e7a16d147
Kleberton Santos <kleberton.santos@sinqia.com.br>
2023-04-24 16:07:06 -0300

    Inicio HackerRank

---
+++ src/Basico/HackerRank1.java
@@ -0,0 +1,9 @@
+package Basico;
+
+public class HackerRank1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+    }
+}
