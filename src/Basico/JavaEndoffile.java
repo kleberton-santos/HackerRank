866ed151ad151721cb330dfd193262deda54b853
Kleberton Santos <kleberton.santos@sinqia.com.br>
2023-04-26 11:20:54 -0300

    Continuando desafios

---
+++ src/Basico/JavaEndoffile.java
@@ -0,0 +1,19 @@
+package Basico;
+
+import java.util.Scanner;
+
+public class JavaEndoffile {
+    public static void main(String[] args) {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner sc = new Scanner(System.in);
+
+            int linha = 0;
+            while (sc.hasNextLine()){
+                linha++;
+                String linhas = sc.nextLine();
+                System.out.println(linha + " " + linhas);
+        }
+
+
+    }
+}
---
+++ src/Basico/JavaStaticInitializerBlock.java
@@ -0,0 +1,20 @@
+package Basico;
+
+import java.util.Scanner;
+
+public class JavaStaticInitializerBlock {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int B = sc.nextInt();
+        int H = sc.nextInt();
+        if(B <= 0 || H <= 0) {
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        } else {
+            int area=B*H;
+            System.out.print(area);
+        }
+
+    }//end of main
+
+}//end of class
