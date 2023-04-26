b9ecbdafc4df9e7d6a59ab76ccc113a959c278ab
Kleberton Santos <kleberton.santos@sinqia.com.br>
2023-04-26 14:26:58 -0300

    Continuando desafios

---
+++ src/Basico/JavaCurrencyFormatter.java
@@ -0,0 +1,37 @@
+package Basico;
+
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaCurrencyFormatter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        // Write your code here.
+
+        NumberFormat formatUs = NumberFormat.getCurrencyInstance(Locale.US);
+        String us = formatUs.format(payment);
+
+        NumberFormat formatChina = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        String china = formatChina.format(payment);
+
+        NumberFormat formatFranca = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        String france = formatFranca.format(payment);
+
+        Locale fomarIndia = new Locale("en", "INDIA");
+        NumberFormat formatIndia = NumberFormat.getCurrencyInstance(fomarIndia);
+        String india = formatIndia.format(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+    }
+}
